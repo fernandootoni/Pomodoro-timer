411370dba8ae280d8b2ef4f6577762f0d12ae4ee
Fernando Otoni de Oliveira <feer.otoni@hotmail.com>
2022-08-22 09:04:07 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+
+import { defaultTheme } from './styles/themes/default'
+import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
+import { CyclesContextProvider } from './contexts/CycleContext'
+
+// // ------LINT-------
+// npx eslint src --ext .ts,.tsx --fix
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
+  )
+}
+
+export default App
